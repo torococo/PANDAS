8bdc8cbfbe2561794f579f7c107462143d89e140
torococo <rafaelrbravo@gmail.com>
2016-11-14 21:07:39 -0500

    another data file location update

--- code/example.py
+++ code/example.py
@@ -11,7 +11,7 @@ def readJson(path):
   return [json.loads(line) for line in open(path)]
 
 
-# listOfDicts=readJson("./pydata-book-master/ch02/usagov_bitly_data2012-03-16-1331923249.txt")
+# listOfDicts=readJson("./data/ch02/usagov_bitly_data2012-03-16-1331923249.txt")
 # print listOfDicts[0]['tz']
 # timeZones=[rec['tz']for rec in listOfDicts if 'tz' in rec]
 # print timeZones
@@ -21,11 +21,11 @@ def readJson(path):
 # plt.show()
 
 unames = ['user_id', 'gender', 'age', 'occupation', 'zip']
-users = pd.read_table('../pydata-book-master/ch02/movielens/users.dat', sep='::', header=None, names=unames)
+users = pd.read_table('../data/ch02/movielens/users.dat', sep='::', header=None, names=unames)
 rnames = ['user_id', 'movie_id', 'rating', 'timestamp']
-ratings = pd.read_table('../pydata-book-master/ch02/movielens/ratings.dat', sep='::', header=None, names=rnames)
+ratings = pd.read_table('../data/ch02/movielens/ratings.dat', sep='::', header=None, names=rnames)
 mnames = ['movie_id', 'title', 'genres']
-movies = pd.read_table('../pydata-book-master/ch02/movielens/movies.dat', sep='::', header=None, names=mnames)
+movies = pd.read_table('../data/ch02/movielens/movies.dat', sep='::', header=None, names=mnames)
 data = pd.merge(pd.merge(ratings, users), movies)
 
 mean_ratings = data.pivot_table('rating', index='title', columns='gender', aggfunc='mean')
