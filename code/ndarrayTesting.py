53522b3537e94db6a0f1db8c82496f42d49f239c
torococo <rafaelrbravo@gmail.com>
2016-11-16 17:22:41 -0500

    progress

--- code/ndarrayTesting.py
+++ code/ndarrayTesting.py
@@ -1,13 +1,23 @@
-import numpy as np
-from numba import njit
+#import numpy as np
+import pandas as pd
+#from numba import njit
 
-x=np.array(["this","is","a","test"],'S4')
-y=np.array(["1",2,3,4,5,6],'i').reshape(3,2)
-print(y.cumsum(0))
+#x=np.array(["this","is","a","test"],'S4')
+#y=np.array(["1",2,3,4,5,6],'i').reshape(3,2)
+#print(y.cumsum(0))
+#
+#
+#@njit
+#def numbaTest(x):
+#  return x+5
 
+#print(numbaTest(10))
 
-@njit
-def numbaTest(x):
-  return x+5
+a=pd.Series(['a','b','c'],[1,2,3])
+b=pd.Series(['d','e','f'],[1,2,3])
+x=pd.DataFrame({'1':a,'2':b})
+print(x)
 
-print(numbaTest(10))
+#x=pd.DataFrame([[1,2,3],[4,5,6]],index=['a','b'],columns=['i','j','k'])
+#y=x.T
+#print(y.ix[0])
